8f9dc161e17cf246f966aad4ddffe2a8db11eeff
intensestaire <98554265+intensestaire@users.noreply.github.com>
2023-09-25 13:30:17 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,179 @@
+let who_won = 0
+let win = true
+images.createBigImage(`
+    . # # . . . . . . .
+    # . . . . . # # . .
+    # . # # . # . . # .
+    # . . # . # . . # .
+    . # # # . . # # . .
+    `).scrollImage(1, 200)
+while (win == true) {
+    images.createImage(`
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . #
+        . . . . #
+        `).showImage(0)
+    basic.pause(200)
+    if (input.buttonIsPressed(Button.A)) {
+        images.createImage(`
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . #
+            . . . . #
+            `).showImage(0)
+        basic.pause(200)
+    } else if (input.buttonIsPressed(Button.B)) {
+        images.createImage(`
+            # . . . .
+            # . . . .
+            . . . . #
+            . . . . #
+            . . . . #
+            `).showImage(0)
+        basic.pause(200)
+    } else if (input.buttonIsPressed(Button.AB)) {
+        images.createImage(`
+            # . . . .
+            # . . . .
+            # . . . #
+            . . . . #
+            . . . . #
+            `).showImage(0)
+        basic.pause(200)
+    } else {
+        images.createImage(`
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . #
+            . . . . #
+            `).showImage(0)
+    }
+    images.createImage(`
+        . # . . .
+        . # . . .
+        . . . . .
+        . . . # .
+        . . . # .
+        `).showImage(0)
+    basic.pause(200)
+    if (input.buttonIsPressed(Button.A)) {
+        images.createImage(`
+            . # . . .
+            . # . . .
+            . # . . .
+            . . . # .
+            . . . # .
+            `).showImage(0)
+        basic.pause(200)
+    } else if (input.buttonIsPressed(Button.B)) {
+        images.createImage(`
+            . # . . .
+            . # . . .
+            . . . # .
+            . . . # .
+            . . . # .
+            `).showImage(0)
+        basic.pause(200)
+    } else if (input.buttonIsPressed(Button.AB)) {
+        images.createImage(`
+            . # . . .
+            . # . . .
+            . # . # .
+            . . . # .
+            . . . # .
+            `).showImage(0)
+        basic.pause(200)
+    } else {
+        images.createImage(`
+            . # . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . # .
+            `).showImage(0)
+    }
+    images.createImage(`
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . . # . .
+        `).showImage(0)
+    basic.pause(200)
+    if (input.buttonIsPressed(Button.A)) {
+        images.createImage(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `).showImage(0)
+        basic.pause(200)
+        win = true
+        who_won = 0
+        break;
+    } else if (input.buttonIsPressed(Button.B)) {
+        images.createImage(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `).showImage(0)
+        basic.pause(200)
+        win = true
+        who_won = 1
+        break;
+    } else if (input.buttonIsPressed(Button.AB)) {
+        images.createImage(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `).showImage(0)
+        basic.pause(200)
+        win = true
+        who_won = 2
+        break;
+    } else {
+        images.createImage(`
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            . . # . .
+            `).showImage(0)
+    }
+    images.createImage(`
+        . . . # .
+        . . . # .
+        . . . . .
+        . # . . .
+        . # . . .
+        `).showImage(0)
+    basic.pause(200)
+    images.createImage(`
+        . . . . #
+        . . . . #
+        . . . . .
+        # . . . .
+        # . . . .
+        `).showImage(0)
+    basic.pause(200)
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `).showImage(0)
+    basic.pause(200)
+}
+basic.forever(function () {
+	
+})
